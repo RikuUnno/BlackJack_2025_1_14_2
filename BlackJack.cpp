22c36c9699eb791a053824987cd406fe4b76b7da
RikuUnno <riku.unno@gmail.com>
2025-01-13 17:26:44 +0900

    プロジェクト ファイルを追加します。

---
+++ BlackJack.cpp
@@ -0,0 +1,101 @@
+﻿#include <iostream> //入出力
+
+#include "Shoe.h"
+#include "Player.h"
+#include "Dealer.h"
+
+using namespace std;
+
+// 
+static void showResult(Person** p)
+{
+	cout << "============================" << endl;
+	cout << "            result          " << endl;
+	cout << "============================" << endl;
+	cout << "============================" << endl;
+	cout << "player" << endl;
+	p[0]->showHand();
+	cout << "============================" << endl;
+	cout << "dealer" << endl;
+	p[1]->showHand();
+	cout << "============================" << endl;
+
+	if (p[0]->getScore() > p[1]->getScore()) {
+		cout << "Player Win!" << endl;
+	}
+	else if (p[0]->getScore() < p[1]->getScore()) {
+		cout << "Player Lose" << endl;
+	}
+	else {
+		cout << "Push" << endl;
+	}
+}
+
+static void showHand(Person* p)
+{
+	//手札の表示
+	cout << "====================" << endl;
+	cout << p->getName() << endl;
+	p->showHand();
+	cout << "====================" << endl;
+
+}
+
+//メイン関数
+int main() {
+
+	srand((unsigned int)time(NULL));
+
+	enum PERSON
+	{
+		DEALER,
+		PLAYER1,
+		PLAYER2,
+		PLAYER3,
+		PLAYER4,
+		PLAYER5
+	};
+
+	//各オブジェクトの生成
+	Shoe shoe;
+	Person* persons[] = { new Dealer("del"), 
+		new Player("play1"), new Player("play2") , new Player("play3") , new Player("play4") , new Player("play5") };
+	const char* name[] = {"player", "dealer"};
+	const int num = sizeof(persons) / sizeof(Person*);
+
+	//----カードの配布 ... 初期
+	cout << "====================" << endl;
+	for (int i = 0; i < num; i++)
+	{
+		persons[i]->hit(&shoe);
+		showHand(persons[i]);
+	}
+	//プレイヤーに2枚目を配布
+	persons[PLAYER1]->hit(&shoe);
+	showHand(persons[PLAYER1]);
+
+	//ディーラーに2枚目を配布
+	persons[DEALER]->hit(&shoe);
+
+
+	// ----　勝負開始
+	//プレイヤーの実行
+	//バーストしているかどうか判別
+	if (persons[PLAYER1]->play(&shoe)) {
+		//バーストせずstandした
+
+		//ディーラーの手札を表示
+		showHand(persons[DEALER]);
+
+		//ディーラーの自動実行
+		persons[DEALER]->play(&shoe);
+
+		//結果の表示
+		showResult(persons);
+	}
+	else {
+		cout << "Burst Player Lose" << endl;
+	}
+	cout << "====================" << endl;
+	return 0;
+}
---
+++ Person.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "common.h"
+
+class Shoe;
+
+class Person
+{
+private:
+	
+	int _hand[HAND_SIZE];	// 理論上これくらい持てばOK...に今回はしちゃう
+	bool _calcUpdate;
+	int _cardNum;
+	int _score;
+
+public:
+	// コンストラクタ
+	Person(const char* name);
+
+	virtual ~Person();
+
+	//スコアの取得(hitされた時のみ再計算される)
+	int getScore();
+
+	//カードの追加
+	void hit(Shoe* shoe);
+
+	//手札を表示
+	void showHand();
+
+	//ターン処理
+	bool play(Shoe* shoe);
+
+	char* getName() const;
+
+protected:
+	virtual void playBase(Shoe* shoe) = 0;	//ターン処理本体
+	char* _name;
+
+};
+
